bbc048cf3156050b2e057eedb57baf360b157940
TidyCoder <157531579+Tidy-Coder@users.noreply.github.com>
2024-02-14 16:09:26 +0100

    Create javascriptfile.js

---
+++ javascriptfile.js
@@ -0,0 +1,12 @@
+function retablirCouleurGratuit(){
+  document.getElementById("cestgratuit").style.color = "#aaa";
+  document.getElementById("cestgratuit").style.background = "#111";
+  document.getElementById("cestgratuit").style.scale = "1";
+  document.getElementById("cestgratuit").style.left = "0";
+}
+function changerCouleurGratuit(){
+  document.getElementById("cestgratuit").style.color = "#111";
+  document.getElementById("cestgratuit").style.background = "orange";
+  document.getElementById("cestgratuit").style.scale = "1.01";
+  document.getElementById("cestgratuit").style.left = "1em";
+}
